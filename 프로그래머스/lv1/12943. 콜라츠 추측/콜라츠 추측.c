12f773d01bddded2491e654e6587a25eb1e1861e
KangPuding <129344903+KangPuding@users.noreply.github.com>
2023-09-18 23:47:24 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.01 ms, Memory: 4.22 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int num) {
+  int answer = 0;
+  long number = num;
+
+    while(number!=1){
+        if(answer==500)
+            return -1;
+        answer++;
+        number = number%2==0 ? number/2 : number*3+1;
+    }
+
+    return answer;
+}
