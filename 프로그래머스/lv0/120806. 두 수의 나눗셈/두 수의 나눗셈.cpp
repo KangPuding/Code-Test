7bfa0ece4aa21ad955535d3aa43b121944b7700a
KangPuding <129344903+KangPuding@users.noreply.github.com>
2023-09-28 15:59:27 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120806. 두 수의 나눗셈/두 수의 나눗셈.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int num1, int num2) {
+    
+    int result = 0;
+    
+    result = ((float)num1 / num2 ) * 1000;
+    
+    return result;
+    
+}
