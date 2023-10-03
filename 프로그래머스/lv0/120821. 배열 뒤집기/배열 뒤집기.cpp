2cc9a1f288507a146396ce0f5b9090cefda72dfa
KangPuding <129344903+KangPuding@users.noreply.github.com>
2023-10-04 01:06:16 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.01 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/lv0/120821. 배열 뒤집기/배열 뒤집기.cpp
+++ 프로그래머스/lv0/120821. 배열 뒤집기/배열 뒤집기.cpp
@@ -5,15 +5,9 @@
 using namespace std;
 
 vector<int> solution(vector<int> num_list) {
-    vector<int> answer;
 
      // 배열을 뒤집는다 
      reverse(num_list.begin(), num_list.end());
     
-    // 뒤집힌 배열을 answer에 복사한다
-    for (int i = 0; i < num_list.size(); i++) {
-        answer.push_back(num_list[i]);
-    }
-    
-    return answer;
+    return num_list;
 }
