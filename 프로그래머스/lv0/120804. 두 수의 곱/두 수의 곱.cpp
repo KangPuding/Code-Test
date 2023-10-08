a0a98f9b2dd0f07a8b7cce31b3e0d72f071128c9
KangPuding <129344903+KangPuding@users.noreply.github.com>
2023-10-09 03:33:44 +0900

    [level 0] Title: 두 수의 곱, Time: 0.01 ms, Memory: 4.23 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120804. 두 수의 곱/두 수의 곱.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int num1, int num2) {
+    
+    int result = 0;
+    
+    result = num1 * num2;
+    
+    return result;
+}
