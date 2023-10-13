a6935643d425aea08c8d354142cb9c55769a4740
KangPuding <129344903+KangPuding@users.noreply.github.com>
2023-10-14 02:11:38 +0900

    [level unrated] Title: 대문자와 소문자, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/unrated/120893. 대문자와 소문자/대문자와 소문자.cpp
+++ 프로그래머스/unrated/120893. 대문자와 소문자/대문자와 소문자.cpp
@@ -6,14 +6,36 @@ using namespace std;
 string solution(string my_string) {
     string answer = "";
     string s = my_string;
-    
+
     for(int i = 0; i < s.size(); i++){
-        if(s[i] < 97)
-        s[i] += 32;
-        else
-        s[i] -= 32;  
+        s[i] = int (s[i]);
     }
-    
-    
-    return s;
+
+    for(int j =0; j < s.size(); j++){
+        if(s[j] > 64 && s[j] <91){
+            s[j] = s[j] + 32;   
+            answer.push_back(static_cast<int>(s[j]));
+        }
+        else if (s[j] > 96 && s[j] < 123){
+            s[j] = s[j] - 32;  
+            answer.push_back(static_cast<int>(s[j]));   
+        }
+    }
+
+    return answer;
 }
+
+// string solution(string my_string) {
+//     string answer = "";
+//     string s = my_string;
+    
+//     for(int i = 0; i < s.size(); i++){
+//         if(s[i] < 97)
+//         s[i] += 32;
+//         else
+//         s[i] -= 32;  
+//     }
+    
+    
+//     return s;
+// }
