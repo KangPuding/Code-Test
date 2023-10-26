d3134c20c2f623b719a238fe39009a93dad3edc6
KangPuding <129344903+KangPuding@users.noreply.github.com>
2023-10-26 21:59:18 +0900

    [level unrated] Title: 마지막 두 원소, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181927. 마지막 두 원소/마지막 두 원소.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> num_list) {
+    vector<int> answer = num_list;
+    
+    reverse(num_list.begin(), num_list.end());
+    
+    if(num_list[0] > num_list[1]){
+       answer.push_back(num_list[0] - num_list[1]);
+    }
+    else if (num_list[0] <= num_list[1]){
+        answer.push_back(num_list[0] * 2);
+    }
+        
+        
+    return answer;
+}
