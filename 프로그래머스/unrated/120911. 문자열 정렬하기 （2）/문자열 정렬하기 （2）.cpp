2d8cf12fd3c46c0513639d5c24cf2fb3d80d4940
KangPuding <129344903+KangPuding@users.noreply.github.com>
2023-12-04 01:10:22 +0900

    [level unrated] Title: 문자열 정렬하기 (2), Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+string solution(string my_string) {
+    string answer = "";
+    
+    for(int i = 0; i < my_string.size(); i++){
+        if('A' <= my_string[i] && 'Z' >= my_string[i])
+            answer.push_back(my_string[i] + 32);
+        else
+            answer.push_back(my_string[i]);
+    }
+    
+     sort(answer.begin(), answer.end(), less<char>());
+    
+    return answer;
+}
