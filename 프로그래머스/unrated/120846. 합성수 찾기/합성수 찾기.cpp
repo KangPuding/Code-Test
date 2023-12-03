a7d2ac06855a8aa5e836e093201ec35074f73903
KangPuding <129344903+KangPuding@users.noreply.github.com>
2023-12-04 01:18:44 +0900

    [level unrated] Title: 합성수 찾기, Time: 0.01 ms, Memory: 4.13 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120846. 합성수 찾기/합성수 찾기.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    int count = 0 ;
+        
+    for(int i =0; i<= n; i++){
+        count = 0;
+    for(int j =1; j<=i; j++){
+        if(i % j ==0)
+            count ++;
+    }
+    if(count >= 3 )
+        answer ++;
+    }
+        return answer;
+    }
