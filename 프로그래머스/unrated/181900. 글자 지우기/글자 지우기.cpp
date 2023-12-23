bfb91fc9465297783b6a459a9053a9d15b4a9ce7
KangPuding <129344903+KangPuding@users.noreply.github.com>
2023-12-23 21:25:42 +0900

    [level unrated] Title: 글자 지우기, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181900. 글자 지우기/글자 지우기.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+string solution(string my_string, vector<int> indices) {
+    string answer = my_string;
+
+    sort(indices.begin(), indices.end(), greater<int>());
+
+    for(int i : indices){
+        answer.erase(answer.begin() + i);
+    }
+
+    return answer;
+}
