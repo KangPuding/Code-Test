e2ae9a303a8192107e8541243de3a60056864097
KangPuding <129344903+KangPuding@users.noreply.github.com>
2024-01-19 18:24:55 +0900

    [level 0] Title: 커피 심부름, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/0/181837. 커피 심부름/커피 심부름.cpp
+++ 프로그래머스/0/181837. 커피 심부름/커피 심부름.cpp
@@ -7,10 +7,16 @@ int solution(vector<string> order) {
     int answer = 0;
     
     for(int i = 0; i < order.size(); i++){
-        if(order[i] == "cafelatte" || order[i] == "cafelattehot" || order[i] == "hotcafelatte" || order[i] == "icecafelatte" || order[i] == "cafelatteice")
+        if(order[i].find("cafelatte") != string::npos)
             answer += 5000;
-        else 
-        answer += 4500;
+        else
+            answer += 4500;
+        
+        // if(order[i] == "cafelatte" || order[i] == "cafelattehot" || order[i] == "hotcafelatte" 
+        //    || order[i] == "icecafelatte" || order[i] == "cafelatteice")
+        //     answer += 5000;
+        // else 
+        // answer += 4500;
     }
     return answer;
 }
