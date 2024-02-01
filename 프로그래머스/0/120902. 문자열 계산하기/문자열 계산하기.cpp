d6e6fe14ac855f496161b4dff832a96f0df2e521
KangPuding <129344903+KangPuding@users.noreply.github.com>
2024-02-01 18:00:52 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.02 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.cpp
+++ 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.cpp
@@ -7,44 +7,23 @@ using namespace std;
 int solution(string my_string) {
     int answer = 0;
     
-    // 공백을 기준으로 입력받기 위해 sstream의 
-    // stringstream 사용
     stringstream s;
     s.str(my_string);
     
-    // 연산자 저장
-    string opt;
-    
-    // 첫 입력은 무조건 숫자이므로 먼저 입력 받는다.
     s >> answer;
-    string temp;
     
-    // temp에 저장하며 계속 확인
-    while(s >> temp)
-    {
-        // 입력받은게 연산자라면, opt에 저장하고 넘어간다.
-        if (temp == "+")
-        {
-            opt = "+";
-            continue;
-        }
+    char ch;
+    int temp = 0;
+    
+    while(s) {
         
-        if(temp == "-")
-        {
-            opt = "-";
-            continue;
-        }
+        if (ch == '+') 
+            answer += temp;
         
-        // 입력받은게 숫자라면 opt에 따라 계속해서
-        // answer에 답을 누적시킨다.
-        if(opt == "+")
-        {
-            answer += stoi(temp);
-        }
-        else if (opt == "-")
-        {
-            answer -= stoi(temp);
-        }
+        else if(ch == '-') 
+            answer -= temp;
+        
+        s >> ch >> temp;
     }
     
     return answer;
