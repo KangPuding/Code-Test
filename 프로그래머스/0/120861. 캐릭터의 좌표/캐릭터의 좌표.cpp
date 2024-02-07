f3ffe1df35f4dfe1671cc4323f13acc053ccdb3d
KangPuding <129344903+KangPuding@users.noreply.github.com>
2024-02-07 17:28:10 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.05 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.cpp
+++ 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.cpp
@@ -6,16 +6,15 @@ using namespace std;
 vector<int> solution(vector<string> keyinput, vector<int> board) {
     vector<int> answer;
     int x = 0, y = 0;
-    int dx = board[0]/2, dy = board[1]/2;
     
     for(int i=0; i<keyinput.size(); i++){
-        if(keyinput[i] == "up" && y + 1 <= dy) 
+        if(keyinput[i] == "up" && y + 1 <= board[1]/2) 
             y += 1;
-        else if(keyinput[i] == "down" && y - 1 >= -dy) 
+        else if(keyinput[i] == "down" && y - 1 >= -board[1]/2) 
             y -= 1;
-        else if(keyinput[i] == "right" && x + 1 <= dx) 
+        else if(keyinput[i] == "right" && x + 1 <= board[0]/2) 
             x += 1;
-        else if(keyinput[i] == "left" && x - 1 >= -dx) 
+        else if(keyinput[i] == "left" && x - 1 >= -board[0]/2) 
             x -= 1;
     }
     
