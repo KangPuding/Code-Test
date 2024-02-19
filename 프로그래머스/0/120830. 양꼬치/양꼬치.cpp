99e70e026c6b1111fb97278f1b5b084da0389e40
KangPuding <129344903+KangPuding@users.noreply.github.com>
2024-02-19 19:49:45 +0900

    [level 0] Title: 양꼬치, Time: 0.01 ms, Memory: 3.63 MB -BaekjoonHub

--- 프로그래머스/0/120830. 양꼬치/양꼬치.cpp
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.cpp
@@ -4,13 +4,6 @@
 using namespace std;
 
 int solution(int n, int k) {
-    int food = n * 12000;
-    int beverage = k * 2000;
     
-    
-    if(n >= 10){
-        beverage -= (n / 10) * 2000;
-    }
-    
-    return food + beverage;
+    return (n * 12000) + (k * 2000) - (n / 10) * 2000;
 }
