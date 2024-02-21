f224c4aabb93c34a99030834aa593142657e7617
KangPuding <129344903+KangPuding@users.noreply.github.com>
2024-02-21 23:02:54 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.cpp
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.cpp
@@ -4,16 +4,20 @@
 using namespace std;
 
 int solution(int price) {
-    int answer = 0;
+//     int answer = 0;
     
-    if (price >= 500000)
-        answer += price - (price * 0.2);
-    else if (price >= 300000)
-        answer += price - (price * 0.1);
-    else if (price >= 100000)
-        answer += price - (price * 0.05);
-    else
-        answer += price;
+//     if (price >= 500000)
+//         answer += price - (price * 0.2);
+//     else if (price >= 300000)
+//         answer += price - (price * 0.1);
+//     else if (price >= 100000)
+//         answer += price - (price * 0.05);
+//     else
+//         answer += price;
     
-    return answer;
+//     return answer;
+    
+    return price >= 500000 ? price * 0.8 :
+           price >= 300000 ? price * 0.9 :
+           price >= 100000 ? price * 0.95 : price;
 }
