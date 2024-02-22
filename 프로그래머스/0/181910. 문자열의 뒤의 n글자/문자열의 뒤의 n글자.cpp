ef7b13d6efbc760887a4b1705c6882a2ef2567d9
KangPuding <129344903+KangPuding@users.noreply.github.com>
2024-02-22 20:45:31 +0900

    [level 0] Title: 문자열의 뒤의 n글자, Time: 0.01 ms, Memory: 3.67 MB -BaekjoonHub

--- 프로그래머스/0/181910. 문자열의 뒤의 n글자/문자열의 뒤의 n글자.cpp
+++ 프로그래머스/0/181910. 문자열의 뒤의 n글자/문자열의 뒤의 n글자.cpp
@@ -5,15 +5,17 @@
 using namespace std;
 
 string solution(string my_string, int n) {
-    string answer = "";
+//     string answer = "";
     
-    reverse(my_string.begin(), my_string.end());
+//     reverse(my_string.begin(), my_string.end());
     
-    for(int i = 0; i < n; i++){
-        answer += my_string[i];
-    }
+//     for(int i = 0; i < n; i++){
+//         answer += my_string[i];
+//     }
     
-    reverse(answer.begin(), answer.end());
+//     reverse(answer.begin(), answer.end());
 
-    return answer;
+//     return answer;
+
+    return my_string.substr(my_string.size() - n);
 }
