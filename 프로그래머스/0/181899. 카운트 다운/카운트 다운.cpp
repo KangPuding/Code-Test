da84c53bdc6b7cb234c127a14778e26966d36988
KangPuding <129344903+KangPuding@users.noreply.github.com>
2024-02-24 20:49:07 +0900

    [level 0] Title: 카운트 다운, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/0/181899. 카운트 다운/카운트 다운.cpp
+++ 프로그래머스/0/181899. 카운트 다운/카운트 다운.cpp
@@ -1,17 +1,23 @@
 #include <string>
 #include <vector>
-#include <algorithm>
+
 
 using namespace std;
 
 vector<int> solution(int start, int end_num) {
     vector<int> answer;
     
-    for(int i = end_num; i <= start; i++){
+    for(int i = start; i >= end_num; i--){
         answer.push_back(i);
     }
+        
+        
+//   algorithm 사용   
+//     for(int i = end_num; i <= start; i++){
+//         answer.push_back(i);
+//     }
     
-    reverse(answer.begin(), answer.end());
+//     reverse(answer.begin(), answer.end());
     
     return answer;
 }
