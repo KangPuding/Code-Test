5be69f758a952baa04e514cc424e65e6eb6438b9
KangPuding <129344903+KangPuding@users.noreply.github.com>
2024-02-28 23:35:11 +0900

    [level 0] Title: 수 조작하기 2, Time: 1.10 ms, Memory: 5.89 MB -BaekjoonHub

---
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(vector<int> numLog) {
+    string answer = "";
+    
+    for(int i = 0; i < numLog.size() - 1; i++){
+        if(numLog[i] + 1 == numLog[i + 1])
+            answer += "w";
+        else if(numLog[i] - 1 == numLog[i + 1])
+            answer += "s";
+        else if(numLog[i] + 10 == numLog[i + 1])
+            answer += "d";
+        else if(numLog[i] - 10 == numLog[i + 1])
+            answer += "a";
+    }
+    return answer;
+}
