619a017407ceee997ff88df1eaedfbd6d6073a77
KangPuding <129344903+KangPuding@users.noreply.github.com>
2024-03-10 23:38:36 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.cpp
+++ 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.cpp
@@ -9,14 +9,11 @@ string solution(string s) {
     
     sort(s.begin(), s.end());
     
-    for(int i = 0; i < s.size(); i++)
-    {
-        // i == 0 이어도 뒷쪽 메모리에는 쓰레기 값이 들어있으므로 비교해도 된다.
-        // 내가 보고 있는 char의 앞 뒤가 같지 않으면 answer에 추가
-        if(s[i] != s[i + 1] && s[i] != s[i - 1])
-        {
+    for(int i = 0; i < s.size(); i++){
+        if(s[i] != s[i - 1] && s[i] != s[i + 1]){
             answer += s[i];
         }
     }
+    
     return answer;
 }
