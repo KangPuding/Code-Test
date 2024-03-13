c3e39f4bea53ec234473016fc2611338f421647e
KangPuding <129344903+KangPuding@users.noreply.github.com>
2024-03-14 08:58:35 +0900

    [level 0] Title: 7의 개수, Time: 0.01 ms, Memory: 4.11 MB -BaekjoonHub

--- 프로그래머스/0/120912. 7의 개수/7의 개수.cpp
+++ 프로그래머스/0/120912. 7의 개수/7의 개수.cpp
@@ -4,45 +4,17 @@
 using namespace std;
 
 int solution(vector<int> array) {
-    int answer = 0;
-    
+    int count = 0;
     for(int i = 0; i < array.size(); i++){
         while(1){
             if(array[i] == 0)
                 break;
-            if(array[i] % 10 == 7)
-                answer++;
             
-            array[i] /= 10;
+            else if (array[i] % 10 == 7)
+                count++;
+            
+            array[i] = array[i] / 10;
         }
     }
-    return answer;
+    return count;
 }
-
-
-// 틀림 
-// #include <string>
-// #include <vector>
-
-// using namespace std;
-
-// int solution(vector<int> array) {
-//     int answer = 0;
-//     vector<int> a;
-//     vector<int> b;
-    
-//     for(int i = 0; i < array.size(); i++){
-//         a.push_back(array[i] % 10);
-//         b.push_back(array[i] / 10);
-        
-//         if (a[i] == 7 && b[i] == 7){
-//             answer+=2;
-//         }
-//         else if (a[i] == 7 || b[i] == 7){
-//             answer++;
-//         }
-//         else
-//             answer = 0;
-//     }
-//     return answer;
-// }
