ebaa809fc3838030e44992e1abf7bfb57d00009d
KangPuding <129344903+KangPuding@users.noreply.github.com>
2024-03-14 08:48:09 +0900

    [level 0] Title: 리스트 자르기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.cpp
+++ 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.cpp
@@ -4,23 +4,29 @@
 using namespace std;
 
 vector<int> solution(int n, vector<int> slicer, vector<int> num_list) {
-    vector<int> answer;
+    
+    vector<int> result;
+        
     int a = slicer[0];
     int b = slicer[1];
     int c = slicer[2];
     
     if(n == 1){
-        answer = vector<int>(num_list.begin(), num_list.begin() + b + 1);
+        for(int i = 0; i <= b; i++)
+            result.push_back(num_list[i]);
     }
-    else if(n == 2){
-        answer = vector<int>(num_list.begin() + a, num_list.end());
+    else if (n == 2){
+       for(int i = a; i < num_list.size(); i++)
+            result.push_back(num_list[i]);
     }
-    else if(n == 3){
-        answer = vector<int>(num_list.begin() + a, num_list.begin() + b +1);
+    else if (n == 3){
+        for(int i = a; i <= b; i++)
+            result.push_back(num_list[i]);
     }
-    else if(n == 4){
+    else if (n == 4){
         for(int i = a; i <= b; i += c)
-            answer.push_back(num_list[i]);
+            result.push_back(num_list[i]);
     }
-    return answer;
+    
+    return result;
 }
