76f073ccff949e01943c30a3a270e810d7ba751f
KangPuding <129344903+KangPuding@users.noreply.github.com>
2024-03-14 09:07:29 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.03 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.cpp
+++ 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.cpp
@@ -5,20 +5,36 @@
 using namespace std;
 
 int solution(string s) {
-    int answer = 0;
+    
+    int result = 0;
+    
     vector<string> a;
     stringstream ss(s);
     string temp;
     
-    while(ss >> temp) 
+    while(ss >> temp)
         a.push_back(temp);
     
-    for(int i = 0; i < a.size(); i++) {
-        if(a[i] == "Z") 
-            answer -= stoi(a[i - 1]);
-        else 
-            answer += stoi(a[i]);
+    
+    for(int i = 0; i < a.size(); i++){
+        if(a[i] == "Z")
+            result -= stoi(a[i - 1]);
+        else
+            result += stoi(a[i]);
     }
-    return answer;
+    
+    return result;    
 }
-//다시 풀것
+
+
+
+
+
+
+
+
+
+
+
+
+
