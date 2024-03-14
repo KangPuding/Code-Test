f161a505e8d9f53d63709c259d4011de017651e4
KangPuding <129344903+KangPuding@users.noreply.github.com>
2024-03-14 09:17:51 +0900

    [level 0] Title: qr code, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/0/181903. qr code/qr code.cpp
+++ 프로그래머스/0/181903. qr code/qr code.cpp
@@ -4,12 +4,13 @@
 using namespace std;
 
 string solution(int q, int r, string code) {
-    string answer = "";
+    
+    string a = "";
     
     for(int i = 0; i < code.size(); i++){
-        if(i % q == r){
-            answer += code[i];
-        }
+        if(i % q == r)
+            a += code[i];
     }
-    return answer;
+    
+    return a;
 }
