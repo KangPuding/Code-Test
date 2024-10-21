1fb4c5bc2bb2fc5c6396e1967157c47067c50a8b
KangPuding <129344903+KangPuding@users.noreply.github.com>
2024-10-22 03:16:59 +0900

    [level 0] Title: 개미 군단, Time: 0.01 ms, Memory: 4.23 MB -BaekjoonHub

--- 프로그래머스/0/120837. 개미 군단/개미 군단.cpp
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.cpp
@@ -4,5 +4,22 @@
 using namespace std;
 
 int solution(int hp) {
-    return hp / 5 + (hp % 5) / 3 + (hp % 5) % 3;;
+    
+   	int nAnt5{};
+	int nAnt3{};
+	int nAnt1{};
+	int nAntCount{};
+	int nRemainHp{};
+    
+    nAnt5 = 5;
+    nAnt3 = 3;
+    nAnt1 = 1;
+    
+    nAntCount += hp / nAnt5;
+	nRemainHp = hp % nAnt5;
+	nAntCount += nRemainHp / nAnt3;
+	nAntCount += nRemainHp % nAnt3;
+    
+    return nAntCount;
+
 }
