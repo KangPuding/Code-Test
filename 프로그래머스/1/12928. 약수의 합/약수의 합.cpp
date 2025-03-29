30fa1dc37e09f407fc40d7f2879febe9bcaf5d52
KangPuding <129344903+KangPuding@users.noreply.github.com>
2025-03-29 15:23:03 +0900

    [level 1] Title: 약수의 합, Time: 0.02 ms, Memory: 4.02 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.cpp
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.cpp
@@ -4,12 +4,13 @@
 using namespace std;
 
 int solution(int n) {
+    int nAnswer{};
     
-    int answer = 0;
-    
-    for(int i = 1; i <= n; i++){
+    for(int i = 1; i <= n; i++)
+    {
         if(n % i == 0)
-            answer += i;
+            nAnswer += i;    
     }
-    return answer;
+    
+    return nAnswer;
 }
