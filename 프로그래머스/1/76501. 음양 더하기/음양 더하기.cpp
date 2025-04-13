30fefa36346516e78e4facbe736bd02010592c6b
KangPuding <129344903+KangPuding@users.noreply.github.com>
2025-04-13 23:33:05 +0900

    [level 1] Title: 음양 더하기, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.cpp
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.cpp
@@ -4,15 +4,14 @@
 using namespace std;
 
 int solution(vector<int> absolutes, vector<bool> signs) {
+    int answer = 0;
     
-    int result = 0;
-    
-    for(int i = 0; i < signs.size(); i++){
+    for(int i = 0; i < absolutes.size(); i++)
+    {
         if(signs[i] == true)
-            result += absolutes[i];
+            answer += absolutes[i];
         else
-            result -= absolutes[i];
+            answer += -1 * absolutes[i];
     }
-    
-    return result;
+    return answer;
 }
