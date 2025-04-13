9dc9effc169552ec81d6e9eece102ef52318229c
KangPuding <129344903+KangPuding@users.noreply.github.com>
2025-04-13 23:18:52 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 4.90 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/1/12934. 정수 제곱근 판별/README.md
+++ 프로그래머스/1/12934. 정수 제곱근 판별/README.md
@@ -1,10 +1,10 @@
 # [level 1] 정수 제곱근 판별 - 12934 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12934?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12934) 
 
 ### 성능 요약
 
-메모리: 4.19 MB, 시간: 0.01 ms
+메모리: 4.14 MB, 시간: 4.90 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 15일 13:03:12
+2025년 04월 13일 23:18:50
 
 ### 문제 설명
 
--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.cpp
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.cpp
@@ -1,15 +1,20 @@
-#include <cmath>
+
+#include <string>
+#include <vector>
+
+using namespace std;
 
 long long solution(long long n) {
     long long answer = -1;
+    long long nPow = 1;
+    long long i;
     
-    // 주어진 수 n이 양의 정수 x의 제곱인지 확인
-    long long x = sqrt(n); // n의 제곱근을 구한다
-    
-    // x가 정수이고, n이 x의 제곱인 경우 x+1의 제곱을 반환한다
-    if (x * x == n) {
-        answer = (x + 1) * (x + 1);
+    for(i = 1; i * i <= n; i++)
+    {
+        if(i * i == n)
+            answer = (nPow +1) * (nPow +1);
+        else
+            nPow++;
     }
-    
     return answer;
 }
