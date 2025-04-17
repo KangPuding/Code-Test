423a9dbfac471416280edc3b550ed1bac57db66f
KangPuding <129344903+KangPuding@users.noreply.github.com>
2025-04-17 16:28:26 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 130.58 ms, Memory: 105 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.cpp
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.cpp
@@ -1,26 +1,21 @@
 #include <vector>
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
-vector<int> solution(vector<int> arr) {
+vector<int> solution(vector<int> arr) 
+{
     vector<int> answer;
     
     answer.push_back(arr[0]);
     
-    for(int i = 0; i < arr.size(); i++){
-        
-        if(answer.back()==arr[i])
-            continue;
-        else
+    for(int i = 1; i < arr.size(); i++)
+    {
+        if(arr[i - 1] != arr[i])
             answer.push_back(arr[i]);
     }
     
+    
     return answer;
-    
-    
-//     arr.erase(unique(arr.begin(), arr.end()),arr.end());
-
-//     vector<int> answer = arr;
-//     return answer;
 }
