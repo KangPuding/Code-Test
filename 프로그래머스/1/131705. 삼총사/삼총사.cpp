16076dcf1f47fb23aee0b0878ef75f2778b1440e
KangPuding <129344903+KangPuding@users.noreply.github.com>
2025-04-21 00:44:08 +0900

    [level 1] Title: 삼총사, Time: 0.01 ms, Memory: 3.68 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.cpp
+++ 프로그래머스/1/131705. 삼총사/삼총사.cpp
@@ -6,10 +6,15 @@ using namespace std;
 int solution(vector<int> number) {
     int answer = 0;
     
-    for(int i = 0; i < number.size() - 2; i++){
-        for(int j = i + 1; j < number.size() - 1; j++){
-            for(int k = j + 1; k < number.size(); k++){
-                if(number[i] + number[j] + number[k] == 0)
+    for(int i = 0; i < number.size(); i++)
+    {
+        for(int j = i + 1; j < number.size(); j++)
+        {
+            for(int z = j + 1; z < number.size(); z++)
+            {
+                int nSum = 0;
+                nSum = number[i] + number[j] + number[z];
+                if(nSum == 0)
                     answer++;
             }
         }
