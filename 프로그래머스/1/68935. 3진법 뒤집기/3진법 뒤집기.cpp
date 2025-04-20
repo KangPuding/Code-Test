941e85e0b854fc9c661d318763c5d2c9e656a0e3
KangPuding <129344903+KangPuding@users.noreply.github.com>
2025-04-21 00:15:41 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.01 ms, Memory: 4.18 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.cpp
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.cpp
@@ -1,26 +1,25 @@
 #include <string>
 #include <vector>
-#include <cmath>
 
 using namespace std;
 
 int solution(int n) {
     int answer = 0;
     
-    string s;
+    string Str = "";
     
-    while(true){
-        s += to_string(n % 3);
-        
-        if(n < 3)
-            break;
-        else
-            n = n/3;   
+    while(n)
+    {
+        Str += n % 3 +'0';
+        n = n / 3;
     }
     
-    for(int i = 0; i < s.size(); i++){
-        answer += pow(3,s.size()-i-1) * (s[i]-'0');
+    for(int i = 0; i < Str[i] != '\0'; i++)
+    {
+        answer = answer * 3 + (Str[i] -'0');
     }
-    
+
     return answer;
 }
+
+    
--- 프로그래머스/1/68935. 3진법 뒤집기/README.md
+++ 프로그래머스/1/68935. 3진법 뒤집기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 3진법 뒤집기 - 68935 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68935?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68935) 
 
 ### 성능 요약
 
-메모리: 4.2 MB, 시간: 0.03 ms
+메모리: 4.18 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 21일 17:08:36
+2025년 04월 21일 00:15:36
 
 ### 문제 설명
 
