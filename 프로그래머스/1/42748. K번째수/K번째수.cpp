6d182bdedbb9888513249ebe726c39232be5c1ee
KangPuding <129344903+KangPuding@users.noreply.github.com>
2025-04-21 14:29:25 +0900

    [level 1] Title: K번째수, Time: 0.02 ms, Memory: 3.67 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.cpp
+++ 프로그래머스/1/42748. K번째수/K번째수.cpp
@@ -6,17 +6,23 @@ using namespace std;
 
 vector<int> solution(vector<int> array, vector<vector<int>> commands) {
     vector<int> answer;
-    vector<int> s;
+    vector<int> arrReuslt;
     
-    for(int i = 0; i < commands.size(); i++){
-        for(int j = commands[i][0] - 1; j < commands[i][1]; j++){
-            s.push_back(array[j]);
+    for(int i = 0; i < commands.size(); i++)
+        {
+            answer.clear();
+            int nFirst = commands[i][0];
+            int nLast = commands[i][1];
+            int nFind = commands[i][2];
+            
+            for(int j = nFirst - 1; j < nLast; j++)
+            {
+                answer.push_back(array[j]);
+            }
+            
+            sort(answer.begin(), answer.end());
+            
+            arrReuslt.push_back(answer[nFind - 1]);
         }
-        
-        sort(s.begin(), s.end());
-        answer.push_back(s[commands[i][2] - 1]);
-        
-        s.clear();
-    }
-    return answer;
+    return arrReuslt;
 }
