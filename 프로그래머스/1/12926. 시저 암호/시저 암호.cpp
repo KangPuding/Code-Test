04ed2105a13bd3f8857408ebc0a1901ddd40aa82
KangPuding <129344903+KangPuding@users.noreply.github.com>
2025-04-21 14:14:49 +0900

    [level 1] Title: 시저 암호, Time: 0.09 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.cpp
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.cpp
@@ -6,15 +6,15 @@ using namespace std;
 string solution(string s, int n) {
     string answer = "";
     
-    for(int i = 0; i < s.size(); i++){
-        if(s[i] != ' '){
-            if(s[i] >= 'a' && s[i] <= 'z')
-                answer += (s[i] - 'a' + n) % 26 + 'a';
-            else
-                 answer += (s[i] - 'A' + n) % 26 + 'A';
-        }
-        else
-            answer += ' ';
+    for(int i = 0; i < s.size(); i++)
+    {
+        
+        if(s[i] == ' ')
+            answer.push_back(' ');
+        else if (s[i] >= 'a' && s[i] <= 'z')
+            answer += (s[i] -'a' + n) % 26 + 'a';
+         else if (s[i] >= 'A' && s[i] <= 'Z')
+            answer += (s[i] -'A' + n) % 26 + 'A';
     }
     return answer;
 }
