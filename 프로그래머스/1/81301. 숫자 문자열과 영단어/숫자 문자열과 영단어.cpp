24838de15cca202921aca5747b5fe4edfc25e1fa
KangPuding <129344903+KangPuding@users.noreply.github.com>
2025-04-21 15:26:21 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.08 ms, Memory: 4.22 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.cpp
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.cpp
@@ -1,17 +1,19 @@
 #include <string>
 #include <vector>
+#include <algorithm>
 #include <regex>
 
 using namespace std;
 
 int solution(string s) {
+    vector<string> numbers = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"};
     
-    string arr[10] = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
-    
-    for(int i = 0; i < 10; i++){
-        s = regex_replace(s, regex(arr[i]), to_string(i));
-            // regex_replace = (바꿀 문자열이 들어있는 전체 문자열, 정규표현식을 무엇을, 무엇으로 바꿀것인지);
+    int answer = 0;
+    regex r;
+    for(int i = 0; i < numbers.size(); i++) {
+        r = numbers[i];
+        s = regex_replace(s, r, to_string(i));
     }
-    
-    return stoi(s);
+    answer = stoi(s);
+    return answer;
 }
