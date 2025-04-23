644821c399751e712eb75e8d0b0c78f6bf7f5253
KangPuding <129344903+KangPuding@users.noreply.github.com>
2025-04-23 15:10:57 +0900

    [level 1] Title: 콜라 문제, Time: 0.04 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.cpp
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.cpp
@@ -5,10 +5,13 @@ using namespace std;
 
 int solution(int a, int b, int n) {
     int answer = 0;
+    int nExchange = 0;
     
-    while(n >= a){
-        answer += n / a * b;
-        n = (n / a * b) + (n % a);
+    while(n >= a)
+    {
+        nExchange = n / a;
+        answer += nExchange * b;
+        n = n - (nExchange * a) + (nExchange * b);
     }
     return answer;
 }
