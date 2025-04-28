a6f2571c2fe800763f812760a033c4de2dfdc473
KangPuding <129344903+KangPuding@users.noreply.github.com>
2025-04-28 14:20:26 +0900

    [level 1] Title: 기사단원의 무기, Time: 85.67 ms, Memory: 3.65 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.cpp
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.cpp
@@ -6,16 +6,23 @@ using namespace std;
 int solution(int number, int limit, int power) {
     int answer = 0;
     
-    for(int i = 1; i <= number; i++){
-        int cnt = 1;
-        for(int j = 1; j <= i / 2; j++){
+    for(int i = 1; i <= number; i++)
+    {
+        int nCount = 0;
+        for(int j = 1; j * j <= i; j++)
+        {
             if(i % j == 0)
-                cnt++;
+            {
+                nCount++;
+                if (j != i / j) 
+                    nCount++;
+            }
         }
-        if(cnt > limit)
+        
+        if(nCount > limit)
             answer += power;
         else
-            answer += cnt;
+            answer += nCount;
     }
     return answer;
 }
