2d666e14902376a790d94f67b633f0def4acb99a
KangPuding <129344903+KangPuding@users.noreply.github.com>
2025-04-28 14:57:30 +0900

    [level 1] Title: 2016년, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.cpp
+++ 프로그래머스/1/12901. 2016년/2016년.cpp
@@ -5,20 +5,21 @@ using namespace std;
 
 string solution(int a, int b) {
     string answer = "";
-    int sum = 0;
-    // 총 336일, 금요일이 1월 1일 
-    // 31일 ( 1, 3, 5, 7, 8, 10, 12)
-    // 30일 ( 4, 6, 9, 11)
-    // 29일 ( 2 )
     
-    string day[] = {"FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU"};
-    int month[] = {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-    
-    for(int i = 0; i < a; i++){
-        sum += month[i];
+    vector<string> DayofTheWeek = {"FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU"};
+    int arMonthDays[12] = {31,29,31,30,31,30,31,31,30,31,30,31};
+
+    int nAllDay = 0;
+    for(int i = 0; i < a - 1; i++)
+    {
+        nAllDay += arMonthDays[i];
     }
-    sum += b - 1;
-    answer = day[sum % 7];
-        
+    
+    nAllDay += b;
+    
+    int nDay = (nAllDay - 1) % 7;
+    
+    answer = DayofTheWeek[nDay];
+    
     return answer;
 }
