5f7d9d2b544b3ddb3395fa99958a9b01fcb3e0b7
KangPuding <129344903+KangPuding@users.noreply.github.com>
2025-04-30 15:01:39 +0900

    [level 1] Title: 소수 만들기, Time: 0.59 ms, Memory: 3.66 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.cpp
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.cpp
@@ -2,28 +2,36 @@
 #include <iostream>
 using namespace std;
 
-bool IsPrime (int num){
-    
-    if(num == 0 || num == 1)
+bool isPrime(int nData)
+{
+    if(nData < 2)
         return false;
     
-    for(int i = 2; i < num / 2; i++){
-        if(num % i == 0)
+    for(int i = 2; i * i <= nData; i++)
+    {
+        if(nData % i == 0)
             return false;
     }
+    
     return true;
 }
 
 int solution(vector<int> nums) {
     int answer = 0;
-    
-    for(int i = 0; i < nums.size(); i++){
-        for(int j = i + 1; j < nums.size(); j++){
-            for(int k = j + 1; k < nums.size(); k++){
-                if(IsPrime((nums[i] + nums[j] + nums[k])) == true)
+    int nVsize = nums.size();
+
+    for(int i = 0; i < nVsize - 2; i++)
+    {
+       for(int j = i + 1; j < nVsize - 1; j++)
+       {
+           for(int w = j + 1; w < nVsize; w++)
+           { 
+               int nSum = nums[i] + nums[j] + nums[w];  
+               if(isPrime(nSum))
                    answer++;
-            }
-        }
+           }
+       }
     }
+
     return answer;
 }
