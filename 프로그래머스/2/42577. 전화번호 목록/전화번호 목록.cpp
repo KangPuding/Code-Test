2ab466173ada3c0711fb82880526239377b96db1
KangPuding <129344903+KangPuding@users.noreply.github.com>
2025-05-15 16:31:35 +0900

    [level 2] Title: 전화번호 목록, Time: 427.94 ms, Memory: 114 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
@@ -1,23 +1,42 @@
 #include <iostream>
 #include <vector>
 #include <string>
-#include <algorithm>
+//#include <algorithm>
+#include <unordered_map>
 
 using namespace std;
 
 bool solution(vector<string> phone_book) {
-	std::sort(phone_book.begin(), phone_book.end());
+    
+    std::unordered_map<std::string, int> hash_map{};
 
-	for (int i = 0; i < phone_book.size() -1; i++)
-	{
-		std::string& cur = phone_book[i];
-		std::string& next = phone_book[i + 1];
+    for (int i = 0; i < phone_book.size(); i++)
+    {
+	    hash_map[phone_book[i]] = 1;
+    }
 
-		if (next.find(cur) == 0)
-			return false;
+    for (int i = 0; i < phone_book.size(); i++)
+    {
+	    std::string Str = "";
+	    for (int j = 0; j < phone_book[i].size(); j++)
+	    {
+	    	Str += phone_book[i][j];
+	    	if (hash_map[Str] && Str != phone_book[i])
+	    		return false;
+    	}
+    }
+    return true;
+    
+	//std::sort(phone_book.begin(), phone_book.end());
 
-	}
-
-
-	return true;
+	//for (int i = 0; i < phone_book.size() -1; i++)
+	//{
+	//	std::string& cur = phone_book[i];
+	//	std::string& next = phone_book[i + 1];
+    //
+	//	if (next.find(cur) == 0)
+	//		return false;
+    //
+	//}
+    // return true;
 }
