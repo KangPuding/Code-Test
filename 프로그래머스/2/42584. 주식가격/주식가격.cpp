6ceb03907cb5a61c565f48ae6f1b7d9786cea580
KangPuding <129344903+KangPuding@users.noreply.github.com>
2025-05-29 18:35:13 +0900

    [level 2] Title: 주식가격, Time: 28.32 ms, Memory: 24.3 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.cpp
+++ 프로그래머스/2/42584. 주식가격/주식가격.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 vector<int> solution(vector<int> prices) {
     
-	vector<int> nResult(prices.size(), 0);
+	vector<int> vResult(prices.size(), 0);
 	vector<int> stack{};
 
 	int nLength = prices.size();
@@ -17,18 +17,61 @@ vector<int> solution(vector<int> prices) {
 		{
 			int nTopIndex = stack.back();
 			stack.pop_back();
-			nResult[nTopIndex] = i - nTopIndex;
+			vResult[nTopIndex] = i - nTopIndex;
 		}
 		stack.push_back(i);
 	}
 
-	for (int i = 0; i < nResult.size(); ++i)
+	for (int i = 0; i < vResult.size(); ++i)
 	{
-		if (nResult[i] == 0 && i != nLength - 1)
+		if (vResult[i] == 0 && i != nLength - 1)
 		{
-			nResult[i] = nLength - i - 1;
+			vResult[i] = nLength - i - 1;
 		}
 	}
     
-    return nResult;
+    return vResult;
     }
+
+// 실패한 코드 (o(n^2))
+// 이유 : queue를 매번 복사를 진행함 그렇기에 효율성이 떨어짐
+//#include <iostream>
+//#include <string>
+//#include <vector>
+//#include <queue>
+//
+//
+//int main()
+//{
+//	std::vector<int> prices{ 1, 2, 1, 2, 1 };
+//	std::vector<int> nResult{};
+//	int nIndex{};
+//
+//	while (nIndex < prices.size())
+//	{
+//		std::queue<int> Que{};
+//		for (int i = nIndex + 1; i < prices.size(); i++)
+//		{
+//			Que.push(prices[i]);
+//		}
+//
+//		int nCount{};
+//		int nFirst = prices[nIndex];
+//
+//		while (!Que.empty())
+//		{
+//			int nCurrent = Que.front();
+//			Que.pop();
+//			nCount++;
+//
+//			if (nCurrent < nFirst)
+//			{
+//				while (!Que.empty())
+//					Que.pop();
+//			}
+//		}
+//		nResult.push_back(nCount);
+//		printf("%d ", nCount);
+//		nIndex++;
+//	}
+//}
