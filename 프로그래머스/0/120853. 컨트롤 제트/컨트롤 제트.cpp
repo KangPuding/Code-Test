e6048cf160ec1193e60e14efd6dfee30226460e5
KangPuding <129344903+KangPuding@users.noreply.github.com>
2026-02-15 21:12:19 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.05 ms, Memory: 3.63 MB -BaekjoonHub

--- 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.cpp
+++ 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.cpp
@@ -1,4 +1,38 @@
+#include <iostream>
 #include <string>
+#include <sstream>
+#include <vector>
+
+using namespace std;
+
+int solution(string s) {
+    int answer = 0;
+    std::vector<int> nNum;
+
+    std::stringstream ss(s);
+    std::string temp;
+
+    while (ss >> temp)
+    {
+        if (temp == "Z") {
+            if (!nNum.empty()) {
+                answer -=nNum.back();
+                nNum.pop_back();
+            }
+        }
+        else {
+            nNum.push_back(stoi(temp));
+            answer += nNum.back();
+        }
+    }
+
+
+    printf("%d", answer);
+    return answer;
+}
+
+
+/*#include <string>
 #include <vector>
 
 using namespace std;
@@ -31,4 +65,4 @@ int solution(string s) {
 
     printf("%d", answer);
     return answer;
-}
+}*/
