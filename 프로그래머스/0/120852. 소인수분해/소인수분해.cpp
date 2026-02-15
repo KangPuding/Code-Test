427c0dc14c80780fa27b8a894e16bd773f88e8bc
KangPuding <129344903+KangPuding@users.noreply.github.com>
2026-02-15 21:38:46 +0900

    [level 0] Title: 소인수분해, Time: 0.04 ms, Memory: 3.81 MB -BaekjoonHub

--- 프로그래머스/0/120852. 소인수분해/소인수분해.cpp
+++ 프로그래머스/0/120852. 소인수분해/소인수분해.cpp
@@ -1,25 +1,28 @@
 #include <string>
 #include <vector>
-#include <algorithm>
+#include <unordered_set>
 
-using namespace std;
 
-vector<int> solution(int n) {
-    vector<int> answer;  
-    int a = 2;  // 소인수 분해를 시작할 수
+std::vector<int> solution(int n) {
+    int nQ = 2;
+    std::vector<int> answer;
+    std::unordered_set<int> answerSet;
 
-    while (n != 1) {  
-        if (n % a == 0) {  
-            if (find(answer.begin(), answer.end(), a) == answer.end()) 
-                answer.push_back(a); 
-            //벡터 answer에 중복된 소인수를 추가하지 않기 위해, find 함수로 현재의 a가 이미 벡터에 있는지 확인합니다. 
-            //만약 없다면, push_back으로 추가합니다.
-
-            n /= a;  
+    while (n >= nQ) {
+        if (n % nQ == 0) {
+            n = n / nQ;
+            if (answerSet.find(nQ) == answerSet.end()) {
+                answerSet.insert(nQ);
+                answer.push_back(nQ);
+            }
         }
-                    else {
-                        a++;  // a가 n의 소인수가 아니면 다음 수로 넘어감
-                    }
-                   }
-    return answer;  
+        else if (n % nQ != 0)
+            nQ++;
+    }
+
+    for (int i = 0; i < answer.size(); i++) {
+        printf("%d", answer[i]);
+    }
+    
+    return answer;
 }
