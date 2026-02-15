2f68f9da3b84c389be1a44d735e88d03e3730bcb
KangPuding <129344903+KangPuding@users.noreply.github.com>
2026-02-15 23:49:51 +0900

    [level 0] Title: 문자 개수 세기, Time: 0.02 ms, Memory: 3.63 MB -BaekjoonHub

--- 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.cpp
+++ 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.cpp
@@ -4,21 +4,15 @@
 
 std::vector<int> solution(std::string my_string) {
 
-    std::vector<int> answer;
-    char cAlpha = 'A';
+    std::vector<int> answer(52,0);
 
-    while (cAlpha < 123) {
-        int nCheck = 0;
-        if (cAlpha == 91)
-            cAlpha = 97;
-
-        for (char c : my_string) {
-            if (c == cAlpha)
-                nCheck++;
-        }
-        answer.push_back(nCheck);
-        cAlpha++;
+    // 미리 52개로 초기화 -> char형으로 비교 -> 각 char에 해당 인덱스 값에 증가 (a의 인덱스 : 26 / A의 인덱스 : 0)
+    for (char c : my_string) {
+        if (c < 'a')
+            answer[c - 'A']++;  
+        else
+            answer[26 + c - 'a']++;
     }
-    
+      
         return answer;
 }
