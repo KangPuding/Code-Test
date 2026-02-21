ecdb5efb5e5014622639b70979c52700f1f4a391
KangPuding <129344903+KangPuding@users.noreply.github.com>
2026-02-21 17:45:14 +0900

    [level 0] Title: 두 수의 합, Time: 7.75 ms, Memory: 4.41 MB -BaekjoonHub

--- 프로그래머스/0/181846. 두 수의 합/두 수의 합.cpp
+++ 프로그래머스/0/181846. 두 수의 합/두 수의 합.cpp
@@ -5,25 +5,77 @@
 using namespace std;
 
 string solution(string a, string b) {
-    string answer; 
-    
-    int i = a.size() - 1;
-    int j = b.size() - 1;
-    int up = 0;
-    
-    while (i >= 0 || j >= 0 || up > 0) {
-        int sum = up;
-        if (i >= 0) {
-            sum += (a[i] - '0');
-            i--;
-        }
-        if (j >= 0) {
-            sum += (b[j] - '0');
-            j--;
-        }
-        up = sum / 10;        
-        answer += to_string(sum % 10);
-    }
-    reverse(answer.begin(), answer.end());
-    return answer;
+   std::string Str{};
+
+   reverse(a.begin(), a.end());
+   reverse(b.begin(), b.end());
+
+   int aSize = a.size();
+   int bSize = b.size();
+
+   int nMax{};
+   int nIndex{};
+   int nSum = 0;
+   char cSum = '0';
+
+   if (aSize > bSize)
+      nMax = aSize;
+   else
+      nMax = bSize;
+
+   char aChar{};
+   char bChar{};
+
+   while (nIndex < nMax)
+   {
+      if (aSize > nIndex)
+         aChar = a[nIndex];
+      else
+         aChar = '0';
+
+      if (bSize > nIndex)
+         bChar = b[nIndex];
+      else
+         bChar = '0';
+
+      if (cSum == '1')
+      {
+         nSum = aChar - '0' + bChar - '0' + 1;
+
+         if (nSum > 9)
+         {
+            cSum = '1';
+            nSum -= 10;
+         }
+         else
+            cSum = '0';
+
+         Str += std::to_string(nSum);
+      }
+      else if (cSum == '0')
+      {
+         nSum = aChar - '0' + bChar - '0';
+
+         if (nSum > 9)
+         {
+            cSum = '1';
+            nSum -= 10;
+         }
+         else
+            cSum = '0';
+
+         Str += std::to_string(nSum);
+      }
+      nIndex++;
+   }
+
+   if (cSum == '1')
+      Str += "1";
+
+   reverse(Str.begin(), Str.end());
+
+   for (int i = 0; i < Str.size(); i++)
+      printf("%c", Str[i]);
+
+    return Str;
 }
