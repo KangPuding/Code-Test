8e5b4f8171b72216047168627e5491d54e57effd
KangPuding <129344903+KangPuding@users.noreply.github.com>
2026-02-21 13:56:04 +0900

    [level 0] Title: 이진수 더하기, Time: 0.02 ms, Memory: 4.18 MB -BaekjoonHub

--- 프로그래머스/0/120885. 이진수 더하기/이진수 더하기.cpp
+++ 프로그래머스/0/120885. 이진수 더하기/이진수 더하기.cpp
@@ -5,35 +5,88 @@
 using namespace std;
 
 string solution(string bin1, string bin2) {
-    string answer = "";
-    int carry=0;
     
+   int nMax{};
+    int nMin{};
+    int nIndex{};
+
     reverse(bin1.begin(), bin1.end());
     reverse(bin2.begin(), bin2.end());
-    
-    if(bin1.size()>bin2.size()){
-        int n=bin1.size()-bin2.size();
-        for(int i=0; i<n; i++)
-            bin2+='0';
+
+    std::string sResult = "";
+    std::string sSum ="0";
+
+    if (bin1.size() > bin2.size()) {
+        nMax = bin1.size();
+        nMin = bin2.size();
     }
-    
-    else if(bin1.size()<bin2.size()){
-        int n=bin2.size()-bin1.size();
-        for(int i=0; i<n; i++)
-            bin1+='0';
+    else {
+        nMax = bin2.size();
+        nMin = bin1.size();
     }
 
-    for(int i=0; i<bin1.size(); i++){
-        int n = (bin1[i]-'0')+(bin2[i]-'0')+carry;
-        carry=n/2;
-        answer+=to_string(n%2);
+    while (nMax > nIndex) {
+
+        char a;
+        char b;
+
+        if (nIndex < bin1.size())
+            a = bin1[nIndex];
+        else
+            a = '0';
+
+        if (nIndex < bin2.size())
+            b = bin2[nIndex];
+        else
+            b = '0';
+
+        if (sSum == "1")
+        {
+            if (a == '1' && b =='1')
+            {
+                sSum = "1";
+                sResult += '1';
+            }
+            else if ((a == '0' && b == '1') || a == '1' && b == '0')
+            {
+                sSum = "1";
+                sResult += '0';
+            }
+            else
+            {
+                sSum = "0";
+                sResult += '1';
+            }
+        }
+        else if (sSum == "0")
+        {
+            if (a == '1' && b == '1')
+            {
+                sSum = "1";
+                sResult += '0';
+            }
+            else if ((a == '0' && b == '1') || a == '1' && b == '0')
+            {
+                sSum = "0";
+                sResult += '1';
+            }
+            else
+            {
+                sSum = "0";
+                sResult += '0';
+            }
+        }
+
+        nIndex++;
     }
-    
-    if(carry!=0) answer+=to_string(carry);
-    
-    reverse(answer.begin(), answer.end());
-    
-    return answer;
+
+    if (sSum == "1")
+        sResult += "1";
+
+    reverse(sResult.begin(), sResult.end());
+
+    for (int i = 0; i < sResult.size(); i++) {
+        printf("%c", sResult[i]);
+    }
+    return sResult;
 }
-
-// 다시 풀것, 솔직히 이해 못함 ㅋ
