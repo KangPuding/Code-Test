626497c1e7b6abc481ed9b1b426dfb9115ab75e1
KangPuding <129344903+KangPuding@users.noreply.github.com>
2026-02-21 17:19:43 +0900

    [level 0] Title: 공 던지기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/0/120843. 공 던지기/공 던지기.cpp
+++ 프로그래머스/0/120843. 공 던지기/공 던지기.cpp
@@ -5,11 +5,15 @@ using namespace std;
 
 int solution(vector<int> numbers, int k) {
     int answer = 1;
+    int nIndex = 0;
+    int nSize = numbers.size();
     
-    for(int i = 0; i < k - 1; i++){
-        answer += 2;
-        if(answer > numbers.size())
-            answer -= numbers.size();
+    for(int i = 0; i < k - 1; i++)
+    {
+        nIndex += 2;
+        if(nSize < nIndex)
+            nIndex = nIndex - nSize ;
     }
-    return answer;
+    
+    return numbers[nIndex];
 }
