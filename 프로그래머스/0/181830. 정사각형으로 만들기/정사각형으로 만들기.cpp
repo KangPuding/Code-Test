74da3513b6099dabe6b44841f5e4dfd4953a13c1
KangPuding <129344903+KangPuding@users.noreply.github.com>
2026-02-23 21:41:03 +0900

    [level 0] Title: 정사각형으로 만들기, Time: 4.08 ms, Memory: 5.98 MB -BaekjoonHub

--- 프로그래머스/0/181830. 정사각형으로 만들기/정사각형으로 만들기.cpp
+++ 프로그래머스/0/181830. 정사각형으로 만들기/정사각형으로 만들기.cpp
@@ -4,14 +4,30 @@
 using namespace std;
 
 vector<vector<int>> solution(vector<vector<int>> arr) {
+        
+    std::vector<std::vector<int>> answer;
+    int nWidth = arr.size();
+    int nHeight = arr[0].size();
+
+    int nMaxSize{};
     
-    int l = arr.size() > arr[0].size() ? arr.size() : arr[0].size();
-    vector<vector<int>> answer(l, vector<int>(l, 0));
+    if (nWidth > nHeight)
+        nMaxSize = nWidth;
+    else
+        nMaxSize = nHeight;
+
+        answer.resize(nMaxSize);
     
-    for(int i = 0; i < arr.size(); i++){
-       for(int j = 0; j < arr[0].size(); j++){
+    for (int i = 0; i < nMaxSize; i++) {
+        for (int j = 0; j < nMaxSize; j++) {
+            answer[i].push_back(0);
+        }
+    }
+    for (int i = 0; i < nWidth; i++) {
+        for (int j = 0; j < nHeight; j++) {
             answer[i][j] = arr[i][j];
         }
     }
+    
     return answer;
 }
