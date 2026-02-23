6d0d6d78b677aed8fa80ac8879b47ef077b91e3a
KangPuding <129344903+KangPuding@users.noreply.github.com>
2026-02-24 06:36:31 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.02 ms, Memory: 4.19 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.cpp
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.cpp
@@ -4,30 +4,36 @@
 using namespace std;
 
 string solution(string s) {
-    string answer = "";
-    bool bWord = true;
     
-    for(int i = 0; i < s.size(); i++)
-    {
-        if(s[i] == ' ')
+        std::string Str{};
+        bool isUppercase = true;
+
+        for (int i = 0; i < s.length(); i++)
         {
-            answer += ' ';
-            bWord = true;
-        }
-        else
-        {
-            if(bWord)
-            {
-                answer += toupper(s[i]);
-                bWord = false;
+            if (s[i] == ' ') {
+                isUppercase = true;
+                Str += ' ';
             }
-            
             else
             {
-                answer += tolower(s[i]);
+                if (isUppercase)
+                {
+                    if (48 <= s[i] && s[i] <= 57)
+                        Str += s[i];
+                    else
+                        Str += toupper(s[i]);
+
+                    isUppercase = false;
+                }
+                else
+                {
+                    Str += tolower(s[i]);
+                }
             }
         }
-    }
+
+        for (int i = 0; i < Str.length(); i++)
+            printf("%c", Str[i]);
     
-    return answer;
+    return Str;
 }
