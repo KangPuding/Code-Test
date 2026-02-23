aec9193da3aca8ef861f1e0747735f0383f41923
KangPuding <129344903+KangPuding@users.noreply.github.com>
2026-02-23 20:05:37 +0900

    [level 0] Title: 구슬을 나누는 경우의 수, Time: 0.02 ms, Memory: 4.19 MB -BaekjoonHub

--- 프로그래머스/0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.cpp
+++ 프로그래머스/0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.cpp
@@ -1,19 +1,45 @@
 #include <string>
 #include <vector>
-#include <cmath>
+#include <numeric>
 
 using namespace std;
 
-double fac (int n){
-    if(n > 1)
-        return fac(n - 1) * n;
-    else
-        return 1;
-}
-
 int solution(int balls, int share) {
-
-    double answer = fac(balls) / (fac(share) * fac(balls - share)) ;
     
-    return round(answer);
+        int nMin{};
+        if (share > balls - share)
+            nMin = balls - share;
+        else
+            nMin = share;
+
+        long long nResult = 1;
+        long long nNumerator{};
+        long long nDenominator{};
+
+        for (int i = 1; i <= nMin; i++)
+        {
+            nNumerator = balls - nMin + i;
+            nDenominator = i;
+            
+            // 1차 약분 진행 (이번 항”에서 서로 공유하는 약수를 먼저 제거해서 숫자 크기를 줄이는 단계)
+            int nG1 = std::gcd(nNumerator, nDenominator);
+            if (nG1 != 1)
+            {
+                nNumerator = nNumerator / nG1;
+                nDenominator = nDenominator / nG1;
+            }
+
+            // 2차 약분 진행 (목적은 분모를 최대한 없애고 곱힘)
+            int nG2 = std::gcd(nResult, nDenominator);
+            if (nG2 != 1)
+            {
+                nResult = nResult / nG2;
+                nDenominator = nDenominator / nG2;
+            }
+
+            // 누적
+            nResult = nResult * nNumerator;
+        }
+    
+    return nResult;
 }
