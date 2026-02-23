98138752bb6274f20e7eb573c516f1dc21dfbf25
KangPuding <129344903+KangPuding@users.noreply.github.com>
2026-02-23 20:16:36 +0900

    [level 0] Title: 그림 확대, Time: 0.14 ms, Memory: 4.22 MB -BaekjoonHub

--- 프로그래머스/0/181836. 그림 확대/그림 확대.cpp
+++ 프로그래머스/0/181836. 그림 확대/그림 확대.cpp
@@ -4,20 +4,21 @@
 using namespace std;
 
 vector<string> solution(vector<string> picture, int k) {
-    vector<string> answer;
     
-    for(int i = 0; i < picture.size(); ++i) {
-        string temp;
-        
-        for(int j = 0; j < picture[i].size(); ++j) {
-            for(int l = 0; l < k; ++l) {
-                temp += picture[i][j];
+        std::vector<std::string> Result{};
+
+        for (int i = 0; i < picture.size(); i++)
+        {
+            std::string Str{};
+            for (int j = 0 ; j < picture[i].size(); j++)
+            {
+                for (int n = 0; n < k; n++)
+                    Str += picture[i][j];
             }
+
+            for (int m = 0; m < k; m++)
+                Result.push_back(Str);
         }
-        for(int j = 0; j < k; ++j) {
-            answer.push_back(temp);
-        }
-    }
     
-    return answer;
+    return Result;
 }
