659e84688643180568044a248b34fd9e0bc08f28
KangPuding <129344903+KangPuding@users.noreply.github.com>
2026-02-24 07:06:05 +0900

    [level 2] Title: 올바른 괄호, Time: 0.36 ms, Memory: 3.95 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.cpp
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.cpp
@@ -5,18 +5,30 @@ using namespace std;
 
 bool solution(string s)
 {
+    int nSize = s.size();
+    bool isTrue = true;
     int nCount = 0;
-    
-    for (int i = 0; i < s.length(); i++)
-    {
-        if (s[i] == '(')
-            nCount++;
-        else if (s[i] == ')')
-            nCount--;
 
-        if (nCount < 0)
-            return false;
+    if (s[0] == ')' || s[nSize - 1] == '(')
+        isTrue = false;
+    else
+    {
+        for (int i = 0; i < nSize; i++)
+        {
+            if (nCount == 0 && s[i] == ')')
+                isTrue = false;
+            else 
+            {
+                if (s[i] == '(')
+                    nCount++;
+                else
+                    nCount--;
+            }
+        }
     }
+
+    if (nCount != 0)
+        isTrue = false;
     
-    return nCount == 0;
+    return isTrue;
 }
