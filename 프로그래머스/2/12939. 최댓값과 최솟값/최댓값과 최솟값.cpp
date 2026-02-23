ac38ab026e442a863d1de9360cc45fe5d716b440
KangPuding <129344903+KangPuding@users.noreply.github.com>
2026-02-23 20:47:10 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.04 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.cpp
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.cpp
@@ -1,33 +1,29 @@
 #include <string>
 #include <vector>
 #include <sstream>
-#include <iostream>
 
 using namespace std;
 
 string solution(string s) {
     string answer = "";
-    vector<int> arData;
-    stringstream sStr(s);
-    int num;
     
-    while(sStr >> num)
+    std::stringstream ss(s);
+    int nNum{};
+    ss >> nNum;
+
+    int nMax = nNum;
+    int nMin = nNum;
+
+    while (ss >> nNum)
     {
-        arData.push_back(num);
-    }
-        
-        
-    int nMax = arData[0];
-    int nMin = arData[0];
-    
-    for(int i = 0; i < arData.size(); i++)
-    {
-        if(nMax < arData[i])
-            nMax = arData[i];
-        else if (nMin > arData[i])
-            nMin = arData[i];
+        if (nNum < nMin)
+            nMin = nNum;
+
+        if (nNum > nMax)
+            nMax = nNum;
     }
 
-    answer += to_string(nMin) + ' ' + to_string(nMax);
+    answer = to_string(nMin) + " " + to_string(nMax);
+    
     return answer;
 }
