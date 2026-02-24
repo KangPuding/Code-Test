b3e470c8e4b6faebc16e6df03cc243c3e1c592d5
KangPuding <129344903+KangPuding@users.noreply.github.com>
2026-02-24 20:22:48 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 0.28 ms, Memory: 3.84 MB -BaekjoonHub

--- 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.cpp
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.cpp
@@ -4,19 +4,41 @@
 using namespace std;
 
 int solution(string t, string p) {
-    int answer = 0;
     
-    for(int i = 0; i <= t.size() - p.size(); i++)
+    int nPsize = p.size();;
+    int nIndex = 0;
+    int nResult = 0;
+
+    printf("%d\n", nPsize);
+
+    if (nPsize == 1)
     {
-        string Str = "";
-        for (int j = i; j < i + p.size(); j++)
-        {
-            Str.push_back(t[j]);
+        for (int i = 0; i < t.size(); i++) {
+            std::string Str{};
+            Str += t[i];
+
+            if (p >= Str)
+                nResult++;
         }
-        
-       if (stoll(Str) <= stoll(p))
-            answer++;
     }
+    else
+    {
+        while (nIndex + nPsize - 1 < t.size())
+        {
+            std::string Str{};
+            for (int i = nIndex; i < nIndex + nPsize; i++)
+            {
+                Str += t[i];
+            }
+
+            if (p >= Str)
+                nResult++;
+
+            nIndex++;
+        }
+    }
+
+    printf("%d\n", nResult);
     
-    return answer;
+    return nResult;
 }
