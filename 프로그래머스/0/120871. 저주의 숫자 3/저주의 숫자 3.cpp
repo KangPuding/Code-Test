23aceed14c26b83b1b0692826728e6f90895a31f
KangPuding <129344903+KangPuding@users.noreply.github.com>
2026-02-25 19:22:04 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.03 ms, Memory: 4.23 MB -BaekjoonHub

--- 프로그래머스/0/120871. 저주의 숫자 3/저주의 숫자 3.cpp
+++ 프로그래머스/0/120871. 저주의 숫자 3/저주의 숫자 3.cpp
@@ -4,13 +4,34 @@
 using namespace std;
 
 int solution(int n) {
-    int answer = 0;
-    
-    for(int i = 1; i <= n; i++){
-        answer++; 
-        
-        while(answer % 3 == 0 || (to_string(answer).find('3') != string::npos))
-            answer++;
+    int nResult{};
+
+    for (int i = 0; i < n; i++)
+    {
+        nResult++;
+        bool isCursed = true;
+
+        while (isCursed) {
+            bool isRemain = (nResult % 3 == 0);
+            bool has = false;
+            int nTemp = nResult;
+            while (nTemp > 0)
+            {
+                int digit = nTemp % 10;
+                if (digit == 3)
+                    has = true;
+
+                nTemp = nTemp / 10;
+            }
+
+            if (isRemain || has)
+                nResult++;
+            else
+                isCursed = false;
+
+            printf("%d\n", nResult);
+        }
     }
-    return answer;
+    printf("%d\n", nResult);
+    return nResult;
 }
