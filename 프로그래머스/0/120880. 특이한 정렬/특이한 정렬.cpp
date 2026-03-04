91d47fa63a47440afd3778f5e4ef488a94ee6963
KangPuding <129344903+KangPuding@users.noreply.github.com>
2026-03-04 12:25:18 +0900

    [level 0] Title: 특이한 정렬, Time: 0.03 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.cpp
+++ 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.cpp
@@ -1,24 +1,42 @@
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 vector<int> solution(vector<int> numlist, int n) {
-    int answer;
+
+        std::vector<std::pair<int,int>> items;
+
+        for (int i = 0; i < numlist.size(); i++)
+        {
+            int nData = numlist[i] - n;
+
+            if (nData < 0)
+                nData *= -1;
+
+            items.push_back(std::make_pair(nData, numlist[i]));
+        }
+
+        auto cmp = [](const std::pair<int,int>& a, const std::pair<int,int>& b) -> bool
+        {
+            if (a.first < b.first)
+                return true;
+            if (a.first > b.first)
+                return false;
+            
+            if (a.second > b.second)
+                return true;
+            
+            return false;
+        };
+
+        std::sort(items.begin(), items.end(), cmp);
+
+        std::vector<int> vResult{};
+        
+        for (int i = 0; i < items.size(); i++)
+            vResult.push_back(items[i].second);
     
-     for(int i = 0; i < numlist.size(); i++) {
-         for(int j = i; j < numlist.size(); j++){
-             if(abs(n - numlist[i]) > abs(n - numlist[j])){
-                 answer = numlist[i];
-                 numlist[i] = numlist[j];
-                 numlist[j] = answer;
-             }
-             if(abs(n - numlist[i]) == abs(n - numlist[j]) && numlist[i] < numlist[j]){
-                 answer = numlist[j];
-                 numlist[j] = numlist[i];
-                 numlist[i] = answer;
-             }
-         }
-     }
-    return numlist;
+    return vResult;
 }
