d3f1fd2b6b013c02e4ae871e6471c92275101543
KangPuding <129344903+KangPuding@users.noreply.github.com>
2026-03-04 19:38:34 +0900

    [level 0] Title: 다항식 더하기, Time: 0.04 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/0/120863. 다항식 더하기/다항식 더하기.cpp
+++ 프로그래머스/0/120863. 다항식 더하기/다항식 더하기.cpp
@@ -1,58 +1,74 @@
 #include <string>
 #include <vector>
+#include <sstream>
 
 using namespace std;
 
 string solution(string polynomial) {
-    string answer = "";
-    int cInteger = 0;
-    int cfrontNumber = 0;
-    string num = "";
-    
-    for(int i = 0; i < polynomial.size(); i++)
-    {
-        char ch = polynomial[i];
 
-        if(isdigit(ch))
-            num += ch;
-        else if (ch == 'x')
+    std::stringstream ss (polynomial);
+    std::string Str{};
+    int nSign = 1;
+
+    int nXData = 0;
+    int nData = 0;
+
+    while (ss >> Str) {
+
+        if (Str == "+")
+            nSign = 1;
+        else if (Str == "-")
+            nSign = -1;
+        else if (Str == "x")
+            nXData += nSign * 1;
+        else if (isdigit(Str[0]))
         {
-            if(num == "")
-                cfrontNumber++;
+            if (Str.back() == 'x')
+                nXData += nSign * stoi(Str.substr(0, Str.size() - 1));
             else
-            {
-                cfrontNumber += stoi(num);
-                num = "";
-            }
+                nData += nSign * stoi(Str);
+
         }
-        else if (ch == '+' || ch == ' ')
-        {
-            if(!num.empty())
-            {
-                cInteger += stoi(num);
-                num = "";
-            }
-        }
-    }    
+    }
     
-    if (!num.empty()) 
-        cInteger += stoi(num);
-    
-    if (cfrontNumber > 0) 
+    std::string s = "+";
+    if(nData < 0)
     {
-        if (cfrontNumber == 1)
-            answer += "x";
-        else 
-            answer += to_string(cfrontNumber) + "x";
+        s = "-";
+        nData *= -1;
+    }
+        
+    if(nXData == 0)
+    {
+        if(s == "-")
+            Str = to_string(-1 * nData);
+        else
+            Str = to_string(nData);
     }
 
-    if (cInteger > 0) 
+    else if(nData != 0)
     {
-        if (!answer.empty()) 
-            answer += " + ";
-       
-        answer += to_string(cInteger);
+   if(nXData == 1)
+        Str = string("x") + " " + s + " " + to_string(nData);
+    else if (nXData == -1)
+        Str = string("-x") + " " + s + " " + to_string(nData);
+    else
+        Str = to_string(nXData) + "x" + " " + s + " " + to_string(nData); 
+    }
+    else if (nData == 0)
+    {
+        if(nXData == 1)
+            Str = "x";
+        else if (nXData == -1)
+            Str = "-x";
+        else
+            Str = to_string(nXData) + "x";
     }
     
-    return answer;
+        
+        
+    
+    return Str;
+    
+    
 }
