e001425d438aafec679d86dd25528293d2e66249
KangPuding <129344903+KangPuding@users.noreply.github.com>
2026-03-05 09:44:44 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.30 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.cpp
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.cpp
@@ -5,24 +5,26 @@
 using namespace std;
 
 string solution(vector<int> food) {
-    string answer = "";
-    string left = "";
-    string right = "";
-    
-    for(int i = 1; i < food.size(); i++)
+    std::string Str{};
+    std::string SResult{};
+
+    for (int i = 1; i < food.size(); i++)
     {
-        int nQuotient = 0;
-        nQuotient = food[i] / 2;
-        
-        for(int j = 0; j < nQuotient; j++)
+        int nIndex = i;
+        int nCount = food[i] / 2;
+
+        if (nCount > 0)
         {
-            left += to_string(i);
+            for (int j = 0; j < nCount; j++)
+                Str += std::to_string(nIndex);
         }
     }
+
+    SResult += Str + "0";
+
+    reverse(Str.begin(), Str.end());
+
+    SResult += Str;
     
-    right = left;
-    reverse(right.begin(), right.end());
-    
-    answer = left + '0' + right;
-    return answer;
+    return SResult;
 }
