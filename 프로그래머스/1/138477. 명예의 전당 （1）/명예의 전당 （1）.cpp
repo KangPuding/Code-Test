89498d2c3992772fd554999416845bf86985c63c
KangPuding <129344903+KangPuding@users.noreply.github.com>
2026-03-05 13:07:01 +0900

    [level 1] Title: 명예의 전당 (1), Time: 0.72 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.cpp
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.cpp
@@ -5,18 +5,30 @@
 using namespace std;
 
 vector<int> solution(int k, vector<int> score) {
-    vector<int> answer;
-    vector<int> nMin;
-    
-    for(int i = 0; i < score.size(); i++)
-    {
-        answer.push_back(score[i]);
-        sort(answer.begin(), answer.end(), greater<int>());
+    std::vector<int> temple{};
+    std::vector<int> Result{};
 
-        if (answer.size() > k)                       
-            answer.pop_back();  
-        
-        nMin.push_back(answer.back()); 
+    int nLength = 0;
+
+    for (int i = 0; i < score.size(); i++)
+    {
+        if (nLength < k)
+        {
+            temple.push_back(score[i]);
+            nLength++;
+        }
+        else
+        {
+            if (temple.back() < score[i])
+            {
+                temple.erase(temple.begin() + k - 1);
+                temple.push_back(score[i]);
+            }
+        }
+
+        sort(temple.begin(), temple.end(), std::greater<int>());
+        Result.push_back(temple.back());
     }
-    return nMin;
+    
+    return Result;
 }
