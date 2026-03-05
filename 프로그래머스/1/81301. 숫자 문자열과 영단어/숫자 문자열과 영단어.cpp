2fb555c6f70388fb2bd4494f9b8a6daa6e68d0ba
KangPuding <129344903+KangPuding@users.noreply.github.com>
2026-03-05 11:45:28 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.cpp
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.cpp
@@ -1,19 +1,77 @@
 #include <string>
 #include <vector>
-#include <algorithm>
-#include <regex>
 
 using namespace std;
 
 int solution(string s) {
-    vector<string> numbers = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"};
-    
-    int answer = 0;
-    regex r;
-    for(int i = 0; i < numbers.size(); i++) {
-        r = numbers[i];
-        s = regex_replace(s, r, to_string(i));
+
+    int nIndex = 0;
+    std::string Str{};
+
+    while (nIndex < s.length())
+    {
+        if (s[nIndex] == 'z') {
+            Str += "0";
+            nIndex += 4;
+        }
+        else if (s[nIndex] == 'o') {
+            Str += "1";
+            nIndex += 3;
+        }
+        else if (s[nIndex] == 't')
+        {
+            if (s[nIndex + 1] == 'w')
+            {
+                Str += "2";
+                nIndex += 3;
+            }
+            else
+            {
+                Str += "3";
+                nIndex += 5;
+            }
+        }
+        else if (s[nIndex] == 'f')
+        {
+            if (s[nIndex + 1] == 'o')
+            {
+                Str += "4";
+                nIndex += 4;
+            }
+            else
+            {
+                Str += "5";
+                nIndex += 4;
+            }
+        }
+        else if (s[nIndex] == 's')
+        {
+            if (s[nIndex + 1] == 'i')
+            {
+                Str += "6";
+                nIndex += 3;
+            }
+            else
+            {
+                Str += "7";
+                nIndex += 5;
+            }
+        }
+        else if (s[nIndex] == 'e')
+        {
+            Str += "8";
+            nIndex += 5;
+        }
+        else if (s[nIndex] == 'n')
+        {
+            Str += "9";
+            nIndex += 4;
+        }
+        else {
+            Str += s[nIndex];
+            nIndex++;
+        }
     }
-    answer = stoi(s);
-    return answer;
+    
+    return stoi(Str);
 }
