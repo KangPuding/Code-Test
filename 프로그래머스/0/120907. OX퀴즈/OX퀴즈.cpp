080ca913d1ce216f94378854f08296243a70eeb8
KangPuding <129344903+KangPuding@users.noreply.github.com>
2026-03-06 12:33:06 +0900

    [level 0] Title: OX퀴즈, Time: 0.06 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/0/120907. OX퀴즈/OX퀴즈.cpp
+++ 프로그래머스/0/120907. OX퀴즈/OX퀴즈.cpp
@@ -1,33 +1,38 @@
 #include <string>
 #include <vector>
-#include<sstream>
+#include <sstream>
 
 using namespace std;
 
 vector<string> solution(vector<string> quiz) {
-        vector<string> answer;
     
-    for(int i = 0; i < quiz.size(); i++)
-    {
-        stringstream ss(quiz[i]);
-        int nFirst = 0;
-        int nSecond = 0;
-        int nThird = 0;
-        char cOperator;
-        char cEqual;
+    std::vector<std::string> vResult{};
+
+    for (int i = 0; i < quiz.size(); i++) {
+        std::stringstream ss(quiz[i]);
+        std::string sFirst{};
+        std::string sSecond{};
+        std::string sResult{};
+        std::string sSign{};
+        std::string sSign2{};
+
+        ss >> sFirst >> sSign >> sSecond >> sSign2 >>sResult;
         
-        ss >> nFirst >> cOperator >> nSecond >> cEqual >> nThird;
-        
-        int nResult = 0;
-        if(cOperator == '+')
-            nResult = nFirst + nSecond;
-        else
-            nResult = nFirst - nSecond;
-        
-        if(nResult == nThird)
-            answer.push_back("O");
-        else
-            answer.push_back("X");
+        if (sSign == "+") 
+        {
+           if (stoi(sResult) == stoi(sFirst) + stoi(sSecond))
+               vResult.push_back("O");
+            else
+                vResult.push_back("X");
+        }
+        else if (sSign == "-") 
+        {
+            if (stoi(sResult) == stoi(sFirst) - stoi(sSecond))
+                vResult.push_back("O");
+            else
+                vResult.push_back("X");
+        }
     }
-    return answer;
+    
+    return vResult;
 }
