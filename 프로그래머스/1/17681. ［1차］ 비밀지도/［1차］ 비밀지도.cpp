352683983a55157ff9f0e7436b67f1275a79444b
KangPuding <129344903+KangPuding@users.noreply.github.com>
2026-03-06 13:10:22 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.04 ms, Memory: 4.18 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.cpp
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.cpp
@@ -1,43 +1,59 @@
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
-string ToBinary(int num, int n)
+void binary(int a, int n, std::string &Str)
 {
-    string sBinary = "";
-    
-    for(int i = 0 ; i < n; i++)
+    Str.assign(n, '0');
+
+    int nIndex = n - 1;
+    while (a > 0)
     {
-        if(num % 2 == 1)
-            sBinary = '1' + sBinary;
+        if (a % 2 == 0)
+            Str[nIndex] = '0';
         else
-            sBinary = '0' + sBinary;
-        
-        num /= 2;
+            Str[nIndex] = '1';
+
+        a = a / 2;
+        nIndex--;
     }
-    return sBinary;
 }
 
-
 vector<string> solution(int n, vector<int> arr1, vector<int> arr2) {
-    vector<string> answer;
-    
-    for(int i = 0; i < arr1.size(); i++)
+
+    std::vector<std::string> sArr1{};
+    std::vector<std::string> sArr2{};
+
+    for (int i = 0; i < arr1.size(); i++)
     {
-        string sBinary1 = ToBinary(arr1[i], n);
-        string sBinary2 = ToBinary(arr2[i], n);
-        string merged = "";
-
-        for(int j = 0; j < n; j++)
-        {
-            if(sBinary1[j] == '1' || sBinary2[j] == '1')
-                merged += '#';
-            else
-                merged += ' ';
-        }
-
-        answer.push_back(merged);
+        std::string Str{};
+        binary(arr1[i],n, Str);
+        sArr1.push_back(Str);
     }
-    return answer;
+
+    for (int i = 0; i < arr2.size(); i++)
+    {
+        std::string Str{};
+        binary(arr2[i],n, Str);
+        sArr2.push_back(Str);
+    }
+
+    std::vector<std::string> sResult{};
+
+    for (int i = 0; i < sArr1.size(); i++)
+    {
+        std::string Str{};
+        for (int j = 0; j < sArr1[i].size(); j++)
+        {
+            if (sArr1[i][j] == '1' || sArr2[i][j] == '1')
+                Str += "#";
+            else
+                Str += " ";
+        }
+        sResult.push_back(Str);
+    }
+    
+    return sResult;
 }
