a90011746658f700386dbc21887a8ec33e65766e
KangPuding <129344903+KangPuding@users.noreply.github.com>
2026-03-10 17:06:08 +0900

    [level 2] Title: 괄호 회전하기, Time: 7.85 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.cpp
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.cpp
@@ -1,47 +1,45 @@
 #include <string>
 #include <vector>
-#include <stack>
 
 using namespace std;
 
-bool isVaild(string s)
-{
-    stack<char> st;
-    
-    for(char c : s)
+int solution(string s) {
+    int nIndex = 0;
+    int nResult{};
+
+
+    while (nIndex < s.size())
     {
-        if(c == '(' || c =='{' || c == '[')
-            st.push(c);
-        else
+        std::string Str{};
+        bool isTrue = true;
+
+        for (int i = 0; i < s.size(); i++)
         {
-            if(st.empty())
-                return false;
-            
-            char top = st.top();
-            if ((c == ')' && top != '(') || 
-                (c == '}' && top != '{') || 
-                (c == ']' && top != '['))
-                return false;
-            
-            st.pop();       
+            if (s[i] == '[' || s[i] == '{' || s[i] == '(')
+                Str += s[i];
+            else if (!Str.empty())
+            {
+                if ((Str.back() == '[' && s[i] == ']') || (Str.back() == '{' && s[i] == '}') || (Str.back() == '(' && s[i] == ')'))
+                    Str.pop_back();
+                else
+                    isTrue = false;
+            }
+            else
+                isTrue = false;
         }
-    }
-    
-    return st.empty();
-}
 
-int solution(string s) 
-{
-    int answer = 0;
-    int len = s.length();
+        if (!Str.empty())
+            isTrue = false;
 
-    for (int i = 0; i < len; ++i) 
-    {
-        if (isVaild(s)) 
-            answer++;
-        
-        s = s.substr(1) + s[0];
+
+        if (isTrue)
+            nResult++;
+
+        char cBackUp = s[0];
+        s.erase(0, 1);
+        s += cBackUp;
+        nIndex++;
     }
 
-    return answer;
+    return nResult;
 }
