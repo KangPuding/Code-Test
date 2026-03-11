9128f075c9eefa4babe9612613bf3c202cea7de5
KangPuding <129344903+KangPuding@users.noreply.github.com>
2026-03-11 15:43:35 +0900

    [level 2] Title: n^2 배열 자르기, Time: 43.09 ms, Memory: 24.2 MB -BaekjoonHub

--- 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.cpp
+++ 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.cpp
@@ -4,14 +4,15 @@
 using namespace std;
 
 vector<int> solution(int n, long long left, long long right) {
-    vector<int> answer;
     
-    for(long long i = left ; i <= right; i++)
-    {
-        int nX = i / n;
-        int nY = i % n;
-        answer.push_back(max(nX,nY) + 1);
+    std::vector<int> nResult{};
+
+    for (long long i = left; i <= right; i++) {
+        int row = i / n;
+        int col = i % n;
+
+        nResult.push_back(std::max(row, col) + 1);
     }
     
-    return answer;
+    return nResult;
 }
