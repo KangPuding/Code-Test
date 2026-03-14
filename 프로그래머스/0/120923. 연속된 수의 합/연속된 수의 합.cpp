2967f0fcbd6abdd76a474c8dd77ce653cef6d9ad
KangPuding <129344903+KangPuding@users.noreply.github.com>
2026-03-14 18:25:15 +0900

    [level 0] Title: 연속된 수의 합, Time: 0.03 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/0/120923. 연속된 수의 합/연속된 수의 합.cpp
+++ 프로그래머스/0/120923. 연속된 수의 합/연속된 수의 합.cpp
@@ -4,32 +4,30 @@
 using namespace std;
 
 vector<int> solution(int num, int total) {
-    vector<int> answer;
-	int nCount = -1000;
-	int nResult{};
+    bool isTrue = true;
+    int nNum = -100;
 
-	while (answer.empty())
-	{
-		nResult = 0;
-		int nTmp = nCount;
-		for (int i = 0; i < num; i++)
-		{
-			nResult += nTmp;
-			nTmp++;
-		}
+    while (isTrue)
+    {
+        int nData = 0;
+        int nIndex = nNum;
+        for (int i = 0; i < num; i++)
+        {
+            nData += nIndex;
+            nIndex++;
+        }
 
-		if (nResult == total)
-		{
-			for (int i = 0; i < num; i++)
-			{
-				answer.push_back(nCount);
-				nCount++;
-			}
-		}
-		else
-			nCount++;
-	}
+        if (nData == total)
+            isTrue = false;
+        else
+            nNum++;
+    }
 
+    std::vector<int> vResult;
     
-    return answer;
+    for (int i = nNum; i < nNum + num; i++) {
+        vResult.push_back(i);
+    }
+    
+    return vResult;
 }
