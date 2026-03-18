d9037a6bf3192b058a5ecdaf4dc2c35769455140
KangPuding <129344903+KangPuding@users.noreply.github.com>
2026-03-18 16:43:16 +0900

    [level 1] Title: 덧칠하기, Time: 0.49 ms, Memory: 5.7 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.cpp
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.cpp
@@ -4,14 +4,44 @@
 using namespace std;
 
 int solution(int n, int m, vector<int> section) {
-    int answer = 0;
-    int nPaint = 0; 
-    
-    for (int i = 0; i < section.size(); i++) {
-        if (section[i] > nPaint) { 
-            answer++;
-            nPaint = section[i] + m - 1;
+
+    std::vector<char> wall(n, 'O');
+
+    for (int i = 0; i < section.size(); i++)
+    {
+        wall[section[i] - 1] = 'X';
+    }
+
+    int nIndex = 0;
+    int nCount = 0;
+
+    if (m == 1)
+        nCount = section.size();
+    else
+    {
+        while (nIndex < n)
+        {
+            if (wall[nIndex] == 'X')
+            {
+                if (nIndex < n - m)
+                {
+                    for (int i = nIndex; i < m + nIndex; i++)
+                    {
+                        wall[i] = 'O';
+                    }
+                }
+                else
+                {
+                    for (int i = nIndex; i < n; i++)
+                    {
+                        wall[i] = 'O';
+                    }
+                }
+                nCount++;
+            }
+            nIndex++;
         }
     }
-    return answer;
+    
+    return nCount;
 }
