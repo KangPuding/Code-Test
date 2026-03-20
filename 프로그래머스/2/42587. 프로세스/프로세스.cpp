b18ec320a4115c4be6b2b78a6e4339ff23fe7e40
KangPuding <129344903+KangPuding@users.noreply.github.com>
2026-03-20 13:38:33 +0900

    [level 2] Title: 프로세스, Time: 0.02 ms, Memory: 3.59 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.cpp
+++ 프로그래머스/2/42587. 프로세스/프로세스.cpp
@@ -1,43 +1,44 @@
 #include <string>
 #include <vector>
 #include <queue>
+#include <algorithm>
 
 using namespace std;
 
 int solution(vector<int> priorities, int location) {
+    
+std::queue<std::pair<int,int>> que;
 
-	std::queue<char> q;
+    for (int i = 0; i < priorities.size(); i++) {
+        que.push(std::make_pair(priorities[i],i));
+    }
 
-	for (int i = 0; i < priorities.size(); i++) 
-	{
-		q.push(i);
-	}
-	
-	int nOrder = 0;
+    sort(priorities.begin(), priorities.end(), std::greater<>());
 
-	while (!q.empty())
-	{
-		int nCurrent = q.front();
-		q.pop();
+    std::vector<int> vResult{};
+    int nIndex = 0;
+    while (!que.empty())
+    {
+        int nQue_Data = que.front().first;
+        int nQue_Index = que.front().second;
+        que.pop();
 
-		bool bHigh = false;
-		for (int i = 0; i < priorities.size(); i++)
-		{
-			if (priorities[i] > priorities[nCurrent])
-				bHigh = true;
-		}
-
-		if (bHigh)
-			q.push(nCurrent);
-		else
-		{
-			nOrder++;
-			priorities[nCurrent] = -1;
-
-			if (nCurrent == location)
-				return nOrder;
-		}
-	}
-
-	return -1;
+        if (priorities[nIndex] == nQue_Data)
+        {
+            vResult.push_back(nQue_Index);
+            nIndex++;
+        }
+        else
+            que.push(std::make_pair(nQue_Data,nQue_Index));
+    }
+    
+    int nResult = 0;
+    for (int i = 0; i < vResult.size(); i++) 
+    {
+        if (location == vResult[i])
+            nResult = i + 1;
+            
+    }
+    
+    return nResult;
 }
