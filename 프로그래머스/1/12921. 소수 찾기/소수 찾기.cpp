c8a3449d651d9779cd3ada7e1dfc99d67ead355a
KangPuding <129344903+KangPuding@users.noreply.github.com>
2026-03-20 14:43:49 +0900

    [level 1] Title: 소수 찾기, Time: 149.16 ms, Memory: 3.8 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.cpp
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.cpp
@@ -3,24 +3,25 @@
 
 using namespace std;
 
+bool isPrime(int n)
+{
+    if (n <= 1)
+        return false;
+
+    for (int i = 2; i * i <= n; i++)
+    {
+        if (n % i == 0)
+            return false;
+    }
+    return true;
+}
+
 int solution(int n) {
-    int answer = 0;
-    // 1 제외, 2의 배수 제외, 3의배수 제외, 5의 배수 제외, 7의 배수 제외 
-    vector<bool> vPrime(n + 1, true);
-    
-    for(int i = 2; i * i <= n; i++)
-    {
-        if(vPrime[i])
-        {
-            for(int j = i * i; j <= n; j += i)
-                vPrime[j] = false;
-        }
+    int nCount = 0;
+
+    for (int i = 2; i <= n; i++) {
+        if (isPrime(i))
+            nCount++;
     }
-    
-    for(int i = 2; i <= n; i++)
-    {
-        if(vPrime[i])
-            answer++;
-    }
-    return answer;
+    return nCount;
 }
