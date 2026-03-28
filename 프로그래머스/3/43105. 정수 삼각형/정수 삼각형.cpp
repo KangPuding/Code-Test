5c7aac0f9a6e3b3e2b898c1981e68f4750357857
KangPuding <129344903+KangPuding@users.noreply.github.com>
2026-03-28 16:04:54 +0900

    [level 3] Title: 정수 삼각형, Time: 0.90 ms, Memory: 8.05 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.cpp
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.cpp
@@ -4,14 +4,41 @@
 using namespace std;
 
 int solution(vector<vector<int>> triangle) {
-    
-    for (int i = triangle.size() - 2; i >= 0; i--) 
+    int nSum = triangle[0][0];
+
+    for (int i = 1; i < triangle.size(); i++)
     {
-        for (int j = 0; j < triangle[i].size(); j++) 
+        for (int j = 0; j < triangle[i].size(); j++)
         {
-            triangle[i][j] += max(triangle[i+1][j], triangle[i+1][j+1]);
+            // 첫번째 일때
+            if (j == 0)
+            {
+                triangle[i][j] += triangle[i - 1][j];
+            }
+            // 마지막 일떄
+            else if (j == triangle[i].size() - 1)
+            {
+                triangle[i][j] += triangle[i - 1][j - 1];
+            }
+            // 나머지 일때
+            else
+            {
+                if (triangle[i - 1][j - 1] > triangle[i - 1][j])
+                    triangle[i][j] += triangle[i - 1][j - 1];
+                else
+                    triangle[i][j] += triangle[i - 1][j];
+            }
         }
     }
+
+    int nSize = triangle.size() - 1;
+    int nMax = triangle[nSize][0];
+
+    for (int i = 1; i < triangle[nSize].size(); i++)
+    {
+        if (nMax < triangle[nSize][i])
+            nMax = triangle[nSize][i];
+    }
     
-    return triangle[0][0];
+    return nMax;
 }
