331a9144ea671884f5920f59659dd8df366fb55b
KangPuding <129344903+KangPuding@users.noreply.github.com>
2026-03-30 17:24:01 +0900

    [level 0] Title: 안전지대, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/0/120866. 안전지대/안전지대.cpp
+++ 프로그래머스/0/120866. 안전지대/안전지대.cpp
@@ -3,38 +3,42 @@
 
 using namespace std;
 
+void Danger(std::vector<std::vector<bool>> &vDanger, int nH, int nW)
+{
+
+    for (int i = nH - 1; i <= nH + 1; i++)
+    {
+        for (int j = nW - 1; j <= nW + 1; j++)
+        {
+            if (i >= 0 && j >= 0 && i < vDanger.size() && j < vDanger[0].size())
+                vDanger[i][j] = false;
+        }
+    }
+}
+
+
 int solution(vector<vector<int>> board) {
-    int n = board.size();
-    vector<vector<int>> danger = board;
-    int answer = 0;
-    
-    for(int i = 0; i < n; i++)
+    int nHeight = board.size();
+    int nWidth = board[0].size();
+
+    std::vector<std::vector<bool>> vDanger(nHeight, std::vector<bool>(nWidth, true));
+
+    for (int i = 0; i < nHeight; i++)
     {
-        for(int j = 0; j < n; j++)
+        for (int j = 0; j < nWidth; j++)
         {
-            if(board[i][j] == 1)
-            {
-                for(int dy = -1; dy < 2; dy++)
-                {
-                    for(int dx = -1; dx < 2; dx++)
-                    {
-                        int nX = j + dx;
-                        int nY = i + dy;
-                        if(nY >= 0 && nY < n && nX >= 0 && nX < n)
-                            danger[nY][nX] = 1;
-                    }
-                }
-            }
+            if (board[i][j] == 1)
+                Danger(vDanger, i, j);
+        }
+    }
+
+    int nCount = 0;
+    for (int i = 0; i < vDanger.size(); i++) {
+        for (int j = 0; j < vDanger[i].size(); j++) {
+            if (vDanger[i][j] == true)
+                nCount++;
         }
     }
     
-    for(int i = 0; i < n; i++)
-    {
-        for(int j = 0; j < n; j++)
-        {
-            if(danger[i][j] == 0)
-                answer++;
-        }
-    }
-    return answer;
+    return nCount;
 }
